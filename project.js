b77e44641aa40d021e7b39c2645ab5c12e7a42d6
Elton Fernando Febraio <114992434+Elton-Fernando@users.noreply.github.com>
2023-02-03 07:21:03 -0300

    Update project.js

--- project.js
+++ project.js
@@ -1,51 +1,51 @@
-const button = document.getElementById('convert-button') // 0- INTERLIGAR O BOTÃO CALCULAR
-const select = document.getElementById('currency-select') // 6- INTERLIGANDO A OPÇAO EURO
-// aqui eu estou buscando o botão ID do button CONVERTER
-// só que pra isso eu tive que criar uma classe ID no button
-const dolar = 5.50 // 0,5- CRIAR UM VALOR PARA O DOLAR
-const euro = 5.90 // 0,5- ----------//-------- O EURO
+const button = document.getElementById('convert-button') 
+const select = document.getElementById('currency-select') 
 
-const convertValues = () => {   // colocar o .VALUE para mostrar apenas o valor digitado no input
-   const inputReais = document.getElementById('input-real').value  // 1- ACESSAR O VALOR QUE MOSTRA NE TELA
-   const realValueText = document.getElementById('real-value-text') // 2- MOSTRAR VALOR DIGITADO NO "VALOR", QUANDO CLICADO
-   const currencyValueText = document.getElementById('currency-value-text') // 3- PARA MOSTRAR MEU RESULTADO NO VALOR DOLAR
-  
+
+const convertValues = async () => { 
+   const inputReais = document.getElementById('input-real').value  
+   const realValueText = document.getElementById('real-value-text') 
+   const currencyValueText = document.getElementById('currency-value-text')
    
-   realValueText.innerHTML = new Intl.NumberFormat("pt-BR",{ // 4-
+   const data = await fetch(" https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json())
+  
+   const dolar = data.USDBRL.high 
+   const euro = data.EURBRL.high 
+
+   realValueText.innerHTML = new Intl.NumberFormat("pt-BR",{ 
     style: "currency",
-    currency: "BRL",                   // 4- TODO ESSES COD: PARA COLOCAR A SIGLA E O VALOR NA IMAGEM VALOR R$10.000,00
- }).format(inputReais);                // 4-
+    currency: "BRL",                  
+ }).format(inputReais);                
   
-   if(select.value ==="US$ Dólar americano"){ // 14- AQUI É PARA MUDAR DE DOLAR PARA EURO QUANDO SELECIONADO
-   currencyValueText.innerHTML = new Intl.NumberFormat("en-US",{ // 5-
+   if(select.value ==="US$ Dólar americano"){ 
+   currencyValueText.innerHTML = new Intl.NumberFormat("en-US",{ 
       style: "currency",
-      currency: "USD",                   // 5- TODO ESSES COD: PARA COLOCAR A SIGLA E O VALOR EM DOLAR NO VALOR DOLAR $1.818,18 
+      currency: "USD",                    
    }).format(inputReais / dolar)
-   }                                   // 5-
+   }                                   
    
-   if(select.value ==="€ Euro"){   // 15- AQUI É PARA MUDAR DE EURO PARA DOLAR QUANDO SELECIONADO                                
+   if(select.value ==="€ Euro"){                                  
    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE",{ // 13-
    style: "currency",
-   currency: "EUR",                   // 13- TODO ESSES COD: PARA COLOCAR A SIGLA E O VALOR NA IMAGEM VALOR € 10.000,00
+   currency: "EUR",                   
 }).format(inputReais / euro)
-}};                                     // 13-
+}};                                    
 
-changeCurrency = () => {                                       // 8 - CRIANDO UM ACESSO AO VALOR EURO
-  const currencyName = document.getElementById("currency-name") // 8 -
-  const currencyImg = document.getElementById('currency-img') // 10 - CRIANDO UM ACESSO ATÉ A IMAGEM DA PAGINA
+changeCurrency = () => {                                       
+  const currencyName = document.getElementById("currency-name") 
+  const currencyImg = document.getElementById('currency-img') 
 
-  if (select.value === 'US$ Dólar americano'){ // 12- SE MEU SELECT FOR IGUAL A US$ Dólar americano, que é o resultado que sai quando selecionado
-    currencyName.innerHTML = "Dólar Americano" // 12.3 - ELE VAI MUDAR PARA "Dólar" MA PAGINA EMBAIXO DA IMAGEM
-    currencyImg.src = "./imagem.projeto/estados-unidos (1) 1.svg " // 12.6 - TROCANDO A IMG DE EURO P/ DOLAR QUANDO SELECIONADA
- }                 // ESSE É O NOME DA IMAGEM NA PASTA DE IMG
+  if (select.value === 'US$ Dólar americano'){ 
+    currencyName.innerHTML = "Dólar Americano" 
+    currencyImg.src = "./imagem.projeto/estados-unidos (1) 1.svg "
+ }                
 
-    if (select.value === '€ Euro'){ // 9- SE MEU SELECT FOR IGUAL A € Euro, que é o resultado que sai quando selecionado
-       currencyName.innerHTML = "Euro" // 9.5 - ELE VAI MUDAR PARA "Euro" MA PAGINA EMBAIXO DA IMAGEM
-       currencyImg.src = "./imagem.projeto/Design sem nome 1.svg " // 11 - TROCANDO A IMG DE DOLAR P/ EURO QUANDO SELECIONADA
-    }                // ESSE É O NOME DA IMAGEM NA PASTA DE IMG
-    convertValues() // 16- ISTO É PARA ALTERAR OS RESULTADOS SOZINHO ASSIM Q TROCAR DE EURO P/ DOLAR
+    if (select.value === '€ Euro'){ 
+       currencyName.innerHTML = "Euro"
+       currencyImg.src = "./imagem.projeto/Design sem nome 1.svg "
+    }               
+    convertValues() 
    }
 
-button.addEventListener('click', convertValues)  // 1.5 - AQUI ELE VAI OUVIR TODA VEZ QUE O BOTÃO FOR CLICADO
-select.addEventListener('change', changeCurrency) // 7 - AQUI ELE ACESSA QUANDO SELECIONA A OPÇÃO EURO
-// chamando minha função criada acima
+button.addEventListener('click', convertValues)  
+select.addEventListener('change', changeCurrency) 
